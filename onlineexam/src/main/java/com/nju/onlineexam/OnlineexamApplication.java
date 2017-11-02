74227430838c78cec67c60dd2cc78a60be9929d4
bingshen(申彬) <bingshen@tencent.com>
2017-11-02 19:50:10 +0800

    init backend proj

---
+++ onlineexam/src/main/java/com/nju/onlineexam/OnlineexamApplication.java
@@ -0,0 +1,12 @@
+package com.nju.onlineexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineexamApplication.class, args);
+	}
+}
